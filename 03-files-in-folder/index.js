531108d2022b778211fc17f7b7e805a386e3fe51
Vadim Kolymbet <3t0n@inbox.ru>
2024-01-24 20:00:42 +0300

    feat: add async await in 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,18 +1,21 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs/promises');
 
-fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
-  if (err) throw err;
-
-  for (let file of files) {
-    fs.stat(path.join(__dirname, 'secret-folder/', file), (err, stat) => {
-      if (err) throw err;
+// дабы не создавать async функцию и запускать ее, можно просто IIFE
+(async () => {
+  try {
+    // возврат именно данных, а не промиса. Promise.resolve(data) или Promise.result
+    const files = await fs.readdir(path.join(__dirname, 'secret-folder'));
+    for (let file of files) {
+      const stat = await fs.stat(path.join(__dirname, 'secret-folder/', file));
       if (!stat.isDirectory())
         console.log(
           `${path.parse(file).name} - ${path.extname(file).slice(1)} - ${
             stat.size
           } bytes`,
         );
-    });
+    }
+  } catch (err) {
+    console.log(err);
   }
-});
+})();
