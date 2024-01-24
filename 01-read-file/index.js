5e4925178a719c6666a37c9e33c7d804f7f76f00
Vadim Kolymbet <3t0n@inbox.ru>
2024-01-24 18:21:26 +0300

    feat: add pipeline to 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,17 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+const { pipeline } = require('stream');
 
 // ясно зачем path, запуск идет из папки выше
 const rs = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
-//console.log(rs.data);
-
-let data = '';
-
-// читаем блоками по 64кб, поэтому надо складывать все вместе куда-то
-rs.on('data', (chunk) => (data += chunk));
-
-rs.on('end', () => console.log(data));
-
-// ловим ошибки
-rs.on('error', (error) => console.log(error.message));
+pipeline(rs, process.stdout, (err) => {
+  if (err) console.log(err.message);
+});
