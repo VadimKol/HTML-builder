5366d9878915a0fd9f4c8c2b60cece7dbc20e7da
Vadim Kolymbet <3t0n@inbox.ru>
2024-01-24 18:24:30 +0300

    refactor: little change to 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,8 +3,10 @@ const path = require('path');
 const { pipeline } = require('stream');
 
 // ясно зачем path, запуск идет из папки выше
-const rs = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
-
-pipeline(rs, process.stdout, (err) => {
-  if (err) console.log(err.message);
-});
+pipeline(
+  fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8'),
+  process.stdout,
+  (err) => {
+    if (err) console.log(err.message);
+  },
+);
