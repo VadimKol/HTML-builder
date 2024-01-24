dd40aae48efe53600da116e9db63db8cc1c54c55
Vadim Kolymbet <3t0n@inbox.ru>
2024-01-24 21:34:30 +0300

    feat: remove fs.access and add more simple logic

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,28 +5,22 @@ const files_copy = path.join(__dirname, 'files-copy');
 
 async function copyDir(src, dest) {
   try {
-    await fs.access(dest);
     await fs.rm(dest, { recursive: true, force: true });
-    mainLogic();
-  } catch (err) {
-    if (err.code === 'ENOENT') {
-      mainLogic();
-    } else throw err;
-  }
 
-  async function mainLogic() {
-    try {
-      await fs.mkdir(dest, { recursive: true });
-      const files = await fs.readdir(src);
-      for (let file of files) {
-        const stat = await fs.stat(path.join(src, '/', file));
-        if (!stat.isDirectory())
-          fs.copyFile(path.join(src, '/', file), path.join(dest, '/', file));
-        else copyDir(path.join(src, '/', file), path.join(dest, '/', file)); // подпапки рекурсивно копируем
-      }
-    } catch (err) {
-      throw err;
+    await fs.mkdir(dest, { recursive: true });
+
+    const files = await fs.readdir(src);
+
+    for (let file of files) {
+      const fileSrc = path.join(src, '/', file);
+      const fileDest = path.join(dest, '/', file);
+      const stat = await fs.stat(fileSrc);
+
+      if (!stat.isDirectory()) fs.copyFile(fileSrc, fileDest);
+      else copyDir(fileSrc, fileDest);
     }
+  } catch (err) {
+    console.log(err);
   }
 }
 copyDir(files, files_copy);
